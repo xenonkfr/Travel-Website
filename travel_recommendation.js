67ccdd25b07e31aaca6d0721ec108874463d8538
kfrabbi <kazifazleerabbi.com>
2024-07-20 23:55:05 -0500

    search functionality done

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,24 +1,34 @@
 
-var homeButton = document.findElementByID('home');
-var aboutButton = document.findElementByID('about');
-var contactButton = document.findElementByID('contact');
+var result = document.querySelector("#destinations");
+var hero = document.querySelector("#hero");
+
 
 document.querySelector(".search-bar button").addEventListener('click',function(){
 	
 	
 	var input = document.querySelector(".search-bar input").value.toLowerCase();
 	
-	fetch('travel_recommendation_api.json')
+	fetch('travel_recommendation_api.json', { cache: 'no-store' })
 		.then( response => response.json())
 		.then( data => {
 			
+			console.log("hey" + data.countries[0]);
+			console.log("hey1" + data.countries[0].cities);
 			
-			console.log(data.beaches);
-			if(input in data){
-				
+			if(input == "temple" || input == "temples"){
+				search(data.temples);
+			}else if(input == "beach" || input == "beaches"){
+				search(data.beaches);
+			}else if(input == "australia" ){
+				search(data.countries[0].cities);
+			}else if(input == "brazil" ){
+				search(data.countries[2].cities);
+			}else if(input == "japan"){
+				search(data.countries[1].cities);
+			}else{
+				search(null);
 			}
-			else
-				console.log("Not");
+				
 			
 		})
 		.catch(error => {
@@ -28,9 +38,34 @@ document.querySelector(".search-bar button").addEventListener('click',function()
 	
 });
 
-document.querySelector(".hero").addEventListener('click',function(){
+
+function search(data){
+	hero.style.display = "none";
+	result.style.display = "";
+	console.log(data);
 	
-});	
-document.querySelector(".destinations").addEventListener('click',function(){
+	var destinations_grid = document.querySelector(".destinations-grid");
+	destinations_grid.innerHTML = "";
 	
-});
+	/* if(data == null)
+		destinations_grid.innerHTML = `<p><b>No result found</b></p>`; */
+	
+	data.forEach((city)=>{
+		console.log(city.name);
+		var mydiv = document.createElement('div');
+		mydiv.classList.add('destination-card');
+		
+		var newhtml = `
+			<img src="images/${city.imageUrl}" >
+			<h3>${city.name}</h3>
+			<p>${city.description}</p>
+		`;
+		
+		mydiv.innerHTML = newhtml;
+		destinations_grid.appendChild(mydiv);
+		
+	});
+	
+	
+	
+}
